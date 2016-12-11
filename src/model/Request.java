8a6af74ad76616d13df6964409721319e78ec061
Skylore <iturchin98@gmail.com>
2016-12-11 13:22:33 +0200

    UserController.sendProductRequest --> confirmed

--- src/model/Request.java
+++ src/model/Request.java
@@ -20,4 +20,29 @@ public class Request {
         this.from = from;
         this.to = to;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Request request = (Request) o;
+
+        if (id != request.id) return false;
+        if (price != request.price) return false;
+        if (product != null ? !product.equals(request.product) : request.product != null) return false;
+        if (from != null ? !from.equals(request.from) : request.from != null) return false;
+        return to != null ? to.equals(request.to) : request.to == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (product != null ? product.hashCode() : 0);
+        result = 31 * result + price;
+        result = 31 * result + (from != null ? from.hashCode() : 0);
+        result = 31 * result + (to != null ? to.hashCode() : 0);
+        return result;
+    }
 }
