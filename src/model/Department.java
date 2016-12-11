22439201c5092146c5091f1a02a17c043568b865
Skylore <iturchin98@gmail.com>
2016-12-11 19:15:54 +0200

    Location getter***

--- src/model/Department.java
+++ src/model/Department.java
@@ -29,15 +29,15 @@ public class Department {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public Location getLocation() {
         return location;
     }
 
-    public void setLocation(Location location) {
-        this.location = location;
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
     }
 }
