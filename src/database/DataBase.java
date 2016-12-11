c3ca5c9dcf6e943805e9fc5461b70585832c6f01
Влад <shalamay.vlad44@gmail.com>
2016-12-11 14:44:38 +0200

    mail

--- src/database/DataBase.java
+++ src/database/DataBase.java
@@ -6,12 +6,14 @@ import model.Request;
 import com.sun.istack.internal.NotNull;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DataBase {
 
     private List<Request> requests = new ArrayList<>();
-    private List<Department> departments = new ArrayList<>();
+    private List<Department> departments = new LinkedList<>();
+
 
     public void addRequest(@NotNull Request request) {
         requests.add(request);
