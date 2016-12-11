228766e95c578fcc0606d21afd378c6ead7bf003
Skylore <iturchin98@gmail.com>
2016-12-11 18:58:07 +0200

    Location creating Factory

---
+++ src/controller/LocationFactory.java
@@ -0,0 +1,20 @@
+package controller;
+
+import geolocation.controller.GoogleMapsAPIImpl;
+import geolocation.controller.Location;
+
+public class LocationFactory {
+
+    public static Location create(String country, String city, String street, String houseNum) {
+
+        Location location = null;
+
+        try {
+        location = new GoogleMapsAPIImpl().findLocation(country, city, street, houseNum);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("wrong argument\nlocation has not been initialized");
+        }
+
+        return location;
+    }
+}
