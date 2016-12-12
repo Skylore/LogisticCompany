2f3c2bdbd965387688aa38149859da10816c20e1
Skylore <iturchin98@gmail.com>
2016-12-12 21:19:55 +0200

    new version of ssl

--- src/gmailApi/SendMailSSL.java
+++ src/gmailApi/SendMailSSL.java
@@ -1,6 +1,5 @@
 package gmailApi;
 import com.sun.istack.internal.NotNull;
-import com.sun.istack.internal.Nullable;
 
 import java.util.Properties;
 import javax.mail.Message;
@@ -14,18 +13,17 @@ import javax.mail.internet.MimeMessage;
 public class SendMailSSL {
 
     public static void sendLetter(@NotNull String receiver, @NotNull String tittle, @NotNull String text) {
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
 
-        Session session = Session.getDefaultInstance(props,
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication("aco17.vlad.ivan.task@gmail.com","aco17password");
+                        return new PasswordAuthentication("aco17.vlad.ivan.task@gmail.com", "aco17password");
                     }
                 });
 
@@ -40,7 +38,7 @@ public class SendMailSSL {
 
             Transport.send(message);
 
-            System.out.println("Letter sent!...");
+            System.out.println("Done");
 
         } catch (MessagingException e) {
             throw new RuntimeException(e);
